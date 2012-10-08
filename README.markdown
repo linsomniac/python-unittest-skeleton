14df1ccc1d4dc44479aa56d064e46056148f6c28
Sean Reifschneider <jafo@tummy.com>
2012-10-08 00:33:44 -0600

    Initial checkin.

---
+++ README.markdown
@@ -0,0 +1,21 @@
+Python Unittest Skeleton
+========================
+
+This is a skeleton Python unittest file to allow it to be grabbed and
+filled in, instead of having to remember the structure.
+
+Usage
+-----
+
+To use:
+
+  * Check out repository.
+
+  * Copy "test/test_skeleton.py" into your "test" directory.
+
+  * Rename "test_skeleton.py" to something more descriptive.  (Note: It
+    should keep the "test_" prefix, so that "make" will
+    run the tests).
+
+  * Edit this file and search for "XXX".
+
---
+++ test/test_skeleton.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+#  XXX  Identifying information about tests here.
+#
+#===============
+#  This is based on a skeleton test file, more informatoin at:
+#
+#     https://github.com/linsomniac/python-unittest-skeleton
+
+raise NotImplementedError('To customize, remove this line and '
+		'customize where it says XXX')
+
+import unittest
+
+class test_XXX_Test_Group_Name(unittest.TestCase):
+	@classmethod
+	def setUp(self):
+		###  code to do setup
+		pass
+
+	def tearDown(self):
+		###  code to do tear down
+		pass
+
+	def test_XXX_Test_Name(self):
+		raise NotImplementedError('Insert test code here.')
+
+unittest.main()
